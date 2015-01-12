be3824be43e09f6086cd1b280abc09e690a84d44
Rene Garcia <rene@generalui.com>
2015-01-11 21:41:48 -0800

    testing json api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,13 @@ class ApplicationController < ActionController::Base
 
 
 	def current_user
-		@current_user = (User.where(facebook_token: request.headers["facebook-token"]).first || User.first )
+		@current_user ||=  (User.where(facebook_token: request.headers["facebook-token"]).first || User.first )
 	end
 	helper_method :current_user
 
 	def user_signed_in?
-		unless current_user.nil?
+		puts current_user
+		if current_user.nil?
 			render json: {message: "User not authenticated"}, :status=>401
 		end
 	end
